ba0a7b04d69b978d920b93289cbc70ef34b76afe
Ruben De Freyne <ruben.defreyne.y0592@student.hogent.be>
2019-12-30 16:41:28 +0100

    Weather settings start

---
+++ Daily-Info/Daily-Info/Controllers/WeatherSettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherSettingsViewController.swift
+//  Daily-Info
+//
+//  Created by Ruben  De Freyne on 30/12/2019.
+//  Copyright © 2019 Ruben  De Freyne. All rights reserved.
+//
+
+import Foundation
